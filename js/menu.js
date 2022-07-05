9a07fc2681efb23e255fa0bd9a404cdf7a0b90e9
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-07-05 16:38:59 +0300

    Adds no-scroll function to menu

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,13 @@
   const mobileMenuRef = document.querySelector('[data-menu]');
 
   menuBtnRef.addEventListener('click', () => {
-    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
+    document.body.classList.toggle('no-scroll');
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
